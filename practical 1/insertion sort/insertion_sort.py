93b52c7b4e76329d2f44f58ff27b9a8388e610b1
ahamedbafan786-png <ahamedbafan786@gmail.com>
2026-08-05 13:44:36 +0530

    Update insertion_sort.py

--- practical 1/insertion sort/insertion_sort.py
+++ practical 1/insertion sort/insertion_sort.py
@@ -7,23 +7,12 @@ Time Complexity:
     - Worst Case: O(n^2)
 
 Space Complexity:
-    - O(1) Auxiliary Space (In-place sorting algorithm)
+    - O(1)
 """
 
 def insertion_sort(arr: list) -> list:
-    """
-    Sorts an array in non-decreasing order using the Insertion Sort algorithm.
-
-    Parameters:
-        arr (list): List of elements to be sorted.
-
-    Returns:
-        list: The sorted list.
-    """
     for i in range(1, len(arr)):
         key = arr[i]
-        # Move elements of arr[0..i-1], that are greater than key,
-        # to one position ahead of their current position
         j = i - 1
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
