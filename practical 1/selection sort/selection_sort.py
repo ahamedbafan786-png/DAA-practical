54310412bd39a541063f3931ae239fc9e846f9e4
ahamedbafan786-png <ahamedbafan786@gmail.com>
2026-08-05 14:03:07 +0530

    Update selection_sort.py

--- practical 1/selection sort/selection_sort.py
+++ practical 1/selection sort/selection_sort.py
@@ -7,27 +7,17 @@ Time Complexity:
     - Worst Case: O(n^2)
 
 Space Complexity:
-    - O(1) Auxiliary Space (In-place sorting algorithm)
+    - O(1) 
 """
 
 def selection_sort(arr: list) -> list:
-    """
-    Sorts an array in non-decreasing order using the Selection Sort algorithm.
 
-    Parameters:
-        arr (list): List of elements to be sorted.
-
-    Returns:
-        list: The sorted list.
-    """
     n = len(arr)
     for i in range(n):
-        # Find the minimum element in remaining unsorted array
         min_idx = i
         for j in range(i + 1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
-        # Swap the found minimum element with the first element
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
