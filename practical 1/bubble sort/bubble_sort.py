22a0b3f29423267e94131db8a2e856327adc7bb1
ahamedbafan786-png <ahamedbafan786@gmail.com>
2026-08-05 12:18:25 +0530

    Update bubble_sort.py

--- practical 1/bubble sort/bubble_sort.py
+++ practical 1/bubble sort/bubble_sort.py
@@ -7,28 +7,17 @@ Time Complexity:
     - Worst Case: O(n^2)
 
 Space Complexity:
-    - O(1) Auxiliary Space (In-place sorting algorithm)
+    - O(1) 
 """
 
 def bubble_sort(arr: list) -> list:
-    """
-    Sorts an array in non-decreasing order using the Bubble Sort algorithm.
-
-    Parameters:
-        arr (list): List of elements to be sorted.
-
-    Returns:
-        list: The sorted list.
-    """
     n = len(arr)
     for i in range(n):
         swapped = False
-        # Last i elements are already in place
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-        # If no elements were swapped in inner loop, array is sorted
         if not swapped:
             break
     return arr
